aec6f579e5bd1b9b6e47a7f255cb6a6ca414a8b1
todary <abanob.todary@gmail.com>
2018-02-05 10:20:31 +0200

    start testing

---
+++ test/BusTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: todary
+ * Date: 17/10/17
+ * Time: 11:35 ص
+ */
+
+use App\Transport\Bus;
+
+/**
+ * Class BusTest
+ */
+class BusTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     *
+     */
+    public function testBus()
+    {
+        $car = new Bus('bus','','','Barcelona',' Gerona Airport','',' No seat assignment');
+
+        $data =["trans_number"=>"bus","seat_number"=>"","from"=>"","to"=>"Barcelona","desc"=>" Gerona Airport"];
+      
+        $this->assertEquals(json_encode($data), $car->getJson());
+    }
+
+
+}
